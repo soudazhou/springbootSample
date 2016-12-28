9dbf6c4f2ff02da235aba7f4fc3bf6db3ec88fc9
Wenxuan <soudazhou@gmail.com>
2016-12-28 21:42:17 +0000

    Use @RequestParam to pass var from URL(example: http://localhost:8082/sample/hi?id=25)

--- src/main/java/com/wen/HelloController.java
+++ src/main/java/com/wen/HelloController.java
@@ -2,10 +2,7 @@ package com.wen;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Wenxuan on 28/12/2016.
@@ -16,8 +13,8 @@ public class HelloController {
     @Autowired
     private Car car;
 
-    @RequestMapping(value = {"/hello","/hi/{id}"}, method = RequestMethod.GET)
-    public String speak(@PathVariable("id") Integer id) {
-        return "ID: "+id+" Car name: "+car.getName()+"  Car Speed: "+car.getSpeed();
+    @RequestMapping(value = {"/hello","/hi"}, method = RequestMethod.GET)
+    public String speak(@RequestParam("id") Integer myId) {
+        return "ID: "+myId+" Car name: "+car.getName()+"  Car Speed: "+car.getSpeed();
     }
 }

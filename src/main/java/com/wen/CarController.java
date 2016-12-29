7e295c67c7308f091f474f2a759636ebebe48cce
Wenxuan <soudazhou@gmail.com>
2016-12-29 21:28:49 +0000

    insert data into mysql db via POST

--- src/main/java/com/wen/CarController.java
+++ src/main/java/com/wen/CarController.java
@@ -2,6 +2,8 @@ package com.wen;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +20,16 @@ public class CarController {
     @GetMapping(value = "/allCars")
     public List<Car> getAllCars() {
         return carRepo.findAll();
+    }
+
+    @PostMapping(value ="/Cars")
+    public Car addCar(@RequestParam("name") String name,
+                         @RequestParam("speed") Integer speed) {
+        Car car = new Car();
+        car.setName(name);
+        car.setSpeed(speed);
+        carRepo.save(car);
+        return car;
     }
 
 
